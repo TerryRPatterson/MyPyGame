9d13542f13cd05415b9d8a4397e335f684e3dfd4
TerryPatterson <Terryp@wegrok.net>
2018-02-13 17:05:26 -0500

    Added round control and Sprite Group.

--- wizard_defense.py
+++ wizard_defense.py
@@ -8,13 +8,21 @@ screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN | pygame.HWSURFACE)
 def main():
     pygame.init()
     origin = (0, 0)
-    player = PlayerCharacter(health=10, power=10, name="Test", image_set="Assets/wizard", pos=(0, 0), move_rate=5)
-    skeleton = Enemy(pos=(screen.get_width(), random.randint(0,screen.get_height())))
-    skeleton.position = (screen.get_width()-skeleton.current_image.get_width(),random.randint(0,screen.get_height()))
+    wizard = SpriteGroup("Assets/wizard")
+    skeleton = SpriteGroup("Assets/skeleton")
+    RoundControl()
+    player = PlayerCharacter(health=10, power=10, name="Test",
+                             image_set=wizard, pos=(0, 0),
+                             move_rate=5)
+    # skeleton.position = (screen.get_width()-skeleton.current_image.get_width(),
+    #                      random.randint(0,
+    #                      screen.get_height()
+    #                      - skeleton.current_image.get_height()))
     done = False
     background = pygame.image.load("Assets/Background.png")
     while not done:
-        screen.blit(pygame.transform.scale(background, screen.get_size()), origin)
+        screen.blit(pygame.transform.scale(background,
+                                           screen.get_size()), origin)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
@@ -52,20 +60,117 @@ def main():
         pygame.display.update()
     pygame.quit()
 
+
 class Character(pygame.sprite.Sprite):
     def __init__(self, health, power, name, image_set, pos, move_rate):
+        """Intializes Character."""
         super().__init__()
         self.health = health
         self.power = power
         self.name = name
         self.image_set = image_set
         self.position = pos
-        self.velocity = (0,0)
-        self.animating =  False
+        self.velocity = (0, 0)
+        self.animating = False
         self.move_rate = move_rate
-        self.current_image = pygame.transform.scale(pygame.image.load("Assets/wizard/idle/1_IDLE_000.png"), (int(screen.get_size()[0]*0.1), int(screen.get_size()[1]*0.1)))
 
-        #Loading sprites
+        # Loading sprites
+        self.current_image = self.image_set.idles[0]
+
+        self.animate()
+
+    def attack(self, target):
+        if self.alive and target.alive:
+            target.health -= self.power
+
+    def move(self, change_vel=(0, 0)):
+        """
+        Used to change the velocity of the player.
+
+        Velocity is applied to player postion every frame.
+        """
+        self.velocity = (self.velocity[0] + change_vel[0], self.velocity[1]
+                         + change_vel[1])
+
+    def update(self):
+        self.position = (self.velocity[0] + self.position[0], self.velocity[1]
+                         + self.position[1])
+        self.update_animation()
+        screen.blit(self.current_image, self.position)
+
+    def animate(self, type="idle", duration=0.5):
+        if type == "death":
+            self.anim_list = self.image_set.death
+            self.animating = True
+
+        if type == "walk":
+            self.anim_list = self.image_set.walking
+            self.animating = True
+
+        if type == "attack":
+            self.animating = True
+            self.anim_list = self.image_set.attacks
+
+        if type == "idle":
+            self.anim_list = self.image_set.idles
+            self.animating = False
+
+        self.start_time = pygame.time.get_ticks()
+        self.stop_time = self.start_time + (duration * 1000)
+        self.frames = len(self.anim_list)
+        self.frame_rate = (duration * 1000) / self.frames
+
+    def update_animation(self):
+        if self.stop_time <= pygame.time.get_ticks():
+            self.animating = False
+            self.animate()
+        else:
+            frame_index = int((pygame.time.get_ticks()-self.start_time)
+                              // self.frame_rate)
+            if frame_index == len(self.anim_list):
+                self.current_image = self.anim_list[-1]
+            else:
+                self.current_image = self.anim_list[frame_index]
+
+
+class PlayerCharacter(Character):
+    def __init__(self, health=10, power=2, name="Player",
+                 image_set=wizard, points=0, lives=3, pos=(0, 0),
+                 vel=(0, 0), move_rate=10):
+        super().__init__(health, power, name, image_set, pos, move_rate)
+        self.points = points
+        self.lives = lives
+
+
+class Enemy(Character):
+    def __init__(self, health=random.randint(1, 10), power=1, name=None,
+                 image_set=skeleton, pos=(0, 0), move_rate=random.randint(1, 3)):
+        super().__init__(health, power, name, image_set, pos, move_rate)
+        print(self.name, self.position)
+        self.move(change_vel=(-move_rate, 0))
+
+
+def import_list(file):
+    output = []
+    open_file = open(file, "r")
+    for line in open_file:
+        if line.strip() not in output:
+            output.append(line.strip())
+    # Removes duplicate entries
+    output = list(set(output))
+    return output
+
+
+def load_image(image):
+    pre_scaled_image = pygame.image.load(image).convert_alpha()
+    scaled_image = pygame.transform.scale(pre_scaled_image,
+                                          (int(screen.get_size()[0]*0.1),
+                                           int(screen.get_size()[1]*0.1)))
+    return scaled_image
+
+
+class SpriteGroup():
+    def __init__(self, image_set):
         self.idles = []
         self.attacks = []
         self.walking = []
@@ -76,95 +181,41 @@ class Character(pygame.sprite.Sprite):
         death_paths = glob.glob(image_set + "/die/*.png")
 
         for image in idle_paths:
-            current_image = pygame.image.load(image).convert_alpha()
-            self.idles.append(pygame.transform.scale(current_image, (int(screen.get_size()[0]*0.1), int(screen.get_size()[1]*0.1))))
+            self.idles.append(load_image(image))
 
         for image in attack_paths:
-            current_image = pygame.image.load(image).convert_alpha()
-            self.attacks.append(pygame.transform.scale(current_image, (int(screen.get_size()[0]*0.1), int(screen.get_size()[1]*0.1))))
+            self.attacks.append(load_image(image))
 
         for image in walk_paths:
-            current_image = pygame.image.load(image).convert_alpha()
-            self.walking.append(pygame.transform.scale(current_image, (int(screen.get_size()[0]*0.1), int(screen.get_size()[1]*0.1))))
+            self.walking.append(load_image(image))
 
         for image in death_paths:
-            current_image = pygame.image.load(image).convert_alpha()
-            self.death.append(pygame.transform.scale(current_image, (int(screen.get_size()[0]*0.1), int(screen.get_size()[1]*0.1))))
+            self.death.append(load_image(image))
 
-        self.animate()
-    def attack(self, target):
-        if self.alive and target.alive:
-            target.health -= self.power
 
-    def move(self, change_vel=(0, 0)):
-        self.velocity = (self.velocity[0] + change_vel[0], self.velocity[1] + change_vel[1])
+class RoundControl():
+    def __init__(self):
+        self.round = 1
+        self.enemies = pygame.sprite.Group()
+        round_start()
+        enemy_list_location = "enemy_names.txt"
+        self.name_list = import_list(enemy_list_location)
 
     def update(self):
-        self.position = (self.velocity[0] + self.position[0], self.velocity[1] + self.position[1])
-        self.update_animation()
-        screen.blit(self.current_image, self.position)
-
-    def animate(self, type="idle", duration=0.5):
-        if type == "death":
-            self.anim_list = self.death
-            self.animating = True
-
-        if type == "walk":
-            self.anim_list = self.walking
-            self.animating = True
-
-        if type == "attack":
-            self.animating = True
-            self.anim_list = self.attacks
-
-        if type == "idle":
-            self.anim_list = self.idles
-            self.animating = False
-
-        self.start_time = pygame.time.get_ticks()
-        self.stop_time = self.start_time + (duration * 1000)
-        self.frames = len(self.anim_list)
-        self.frame_rate = (duration * 1000) / self.frames
-
-
-    def update_animation(self):
-        if self.stop_time <= pygame.time.get_ticks():
-            self.animating = False
-            self.animate()
+        if self.enemies:
+            self.enemies.update()
         else:
-            frame_index = int((pygame.time.get_ticks()-self.start_time) // self.frame_rate)
-            if frame_index == len(self.anim_list):
-                self.current_image = self.anim_list[-1]
-            else:
-                self.current_image = self.anim_list[frame_index]
+            self.round += 1
+            self.round_start()
 
-
-class PlayerCharacter(Character):
-    def __init__(self, health=10, power=2, name="Player", image_set="Assets/wizard/", points=0, lives=3, pos=(0, 0), vel=(0 ,0), move_rate=10):
-        super().__init__(health, power, name, image_set, pos, move_rate)
-        self.points = points
-        self.lives = lives
-
-class Enemy(Character):
-    def __init__(self, health=random.randint(1,10), power=1, name=None, image_set="Assets/skeleton",pos=(0,0), move_rate=5):
-        super().__init__(health, power, name, image_set, pos,move_rate)
-        enemy_list_location = "enemy_names.txt"
-        name_list = import_list(enemy_list_location)
-        self.name = random.choice(name_list)
-        print(self.name,self.position)
-
-
-def import_list(file):
-    output = []
-    open_file = open(file, "r")
-    for line in open_file:
-        if line.strip() not in output:
-            output.append(line.strip())
-    #Removes duplicate entries
-    output = list(set(output))
-    return output
-
-
+    def round_start(self):
+        number_of_enemies = random.randint(1, self.round)
+        while number_of_enemies > 0:
+            enemy = Enemy(pos=(screen.get_width(), random.randint(0,
+                               screen.get_height())),
+                          name=random.choice(self.name_list))
+            self.enemies.add(enemy)
+            number_of_enemies -= 1
 
 
 main()
